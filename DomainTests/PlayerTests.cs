6c7865aef9b63821d66d0082d6844cc010cbef49
Nikita Starichenko <n.starichenko@dodopizza.com>
2018-07-03 17:13:21 +0300

    new tests

--- DomainTests/PlayerTests.cs
+++ DomainTests/PlayerTests.cs
@@ -66,5 +66,48 @@ namespace DomainTests
             
             Assert.Equal(false, player.IsInGame);
         }
+        
+        [Fact]
+        [Description("Я, как игрок, могу купить фишки у казино, чтобы делать ставки")]
+        public void ShouldHasChip_WhenBuy()
+        {
+            var player = new Player();
+            var chip = new Chip(10);
+            
+            player.Buy(chip);
+            var hasChip = player.Has(chip);
+            
+            Assert.Equal(true, hasChip);
+        }
+        
+        [Fact]
+        [Description("Я, как игрок, могу сделать ставку в игре в кости, чтобы выиграть")]
+        public void ShouldHasCurrentBet_WhenBet()
+        {
+            var player = new Player();
+            var chip = new Chip(10);
+            var bet = new Bet(chip, 10);
+            
+            player.Bet(bet);
+            
+            Assert.Equal(bet.Chips, player.CurrentBet.Chips);
+            Assert.Equal(bet.Score, player.CurrentBet.Score);
+        }
+        
+        [Fact]
+        [Description("Я, как игрок, не могу поставить фишек больше, чем я купил")]
+        public void ShouldntBet_WhenHasNotEnoughChips()
+        {
+            var player = new Player();
+            var chip = new Chip(10);
+            player.Buy(chip);
+            
+            chip = new Chip(20);
+            var bet = new Bet(chip, 10);
+            
+            player.Bet(bet);
+            
+            Assert.Equal(null, player.CurrentBet);
+        }
     }
 }
