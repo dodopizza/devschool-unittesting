9b55c81b182e887086cf55c30df2a9dfac9a915f
Xneg <xnegxneg@gmail.com>
2018-07-03 15:43:14 +0300

    * Подключил тестируемый проект. * Написал тест на вход в игру.

--- Domain.Tests/PlayerTests.cs
+++ Domain.Tests/PlayerTests.cs
@@ -5,16 +5,21 @@ namespace Domain.Tests
 {
     public class PlayerTests
     {
-        //Я, как игрок, могу войти в игру
         //Я, как игрок, могу выйти из игры
         //Я, как игрок, не могу выйти из игры, если я в нее не входил
         //Я, как игрок, могу играть только в одну игру одновременно
         //Я, как игра, не позволяю войти более чем 6 игрокам
 
         [Fact]
-        public void Test1()
+        //Я, как игрок, могу войти в игру
+        public void PlayerCanJoinGame()
         {
+            var testedPlayer = new Player();
+            var customGame = new RollDiceGame();
 
+            testedPlayer.Join(customGame);
+
+            Assert.True(testedPlayer.IsInGame);
         }
     }
 }
