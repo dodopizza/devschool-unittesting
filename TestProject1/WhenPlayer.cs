c2bf0eb36b10cc5ccdf1958d06cfe709c30067d2
Boris Gulay <b.gulay@dodopizza.com>
2019-04-02 11:00:27 +0300

    Дописали тесты

--- TestProject1/WhenPlayer.cs
+++ TestProject1/WhenPlayer.cs
@@ -7,30 +7,56 @@ namespace TestProject1
     public class WhenPlayer
     {
         [Fact]
-        public void StartsGame_IsInGame()
+        public void StartsGame_ItIsInGame()
         {
             var player = new Player();
+
             player.Join(new RollDiceGame());
+
             Assert.True(player.IsInGame);
         }
 
         [Fact]
-        public void IsInGame_CanNotStartsGameAgain()
+        public void IsInGame_ItCanNotStartsGameAgain()
         {
             var player = new Player();
             player.Join(new RollDiceGame());
+
             Assert.Throws<InvalidOperationException>(
                 () => player.Join(new RollDiceGame())
             );
         }
 
         [Fact]
-        public void EndsGame_NoMoreInGame()
+        public void EndsGame_ItIsNoMoreInGame()
         {
             var player = new Player();
             player.Join(new RollDiceGame());
+
             player.LeaveGame();
+
             Assert.False(player.IsInGame);
+        }
+
+        [Fact]
+        public void Buys10Chips_ItHasMoreThen5Chips()
+        {
+            var player = new Player();
+
+            player.Buy(new Chip(10));
+
+            Assert.True(player.Has(new Chip(5)));
+        }
+
+        [Fact]
+        public void MakeBet_ItHasCurrentBet()
+        {
+            var player = new Player();
+
+            var bet = new Bet(new Chip(5), 13);
+            player.Bet(bet);
+
+            Assert.Equal(bet, player.CurrentBet);
         }
     }
 }
